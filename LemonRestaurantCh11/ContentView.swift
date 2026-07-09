47936dd34ca56d13080d0a255462f42a0828a3f2
Steph <stephtruitt8@gmail.com>
2026-07-09 11:50:29 -0400

    Initial Commit

---
+++ LemonRestaurantCh11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LemonRestaurantCh11
+//
+//  Created by Stephen Truitt on 7/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LemonRestaurantCh11/LemonRestaurantCh11App.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurantCh11App.swift
+//  LemonRestaurantCh11
+//
+//  Created by Stephen Truitt on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurantCh11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
