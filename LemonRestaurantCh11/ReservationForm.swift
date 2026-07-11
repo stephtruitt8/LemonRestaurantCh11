591eade7bb8c13582d3bb77612a8d610a05d4eea
Steph <stephtruitt8@gmail.com>
2026-07-11 17:26:29 -0400

    conditionals

---
+++ LemonRestaurantCh11/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  LemonRestaurantCh11
+//
+//  Created by Stephen Truitt on 7/11/26.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
