f7b7b8dc4801fcb3ae8ff21c5691dc7c7e16503e
Steph <stephtruitt8@gmail.com>
2026-07-22 21:25:09 -0700

    commit

---
+++ LemonRestaurantCh11/DataListView.swift
@@ -0,0 +1,18 @@
+//
+//  DataListView.swift
+//  LemonRestaurantCh11
+//
+//  Created by Stephen Truitt on 7/22/26.
+//
+
+import SwiftUI
+
+struct DataListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataListView()
+}
---
+++ LemonRestaurantCh11/DrinkSizeSwitcher.swift
@@ -0,0 +1,18 @@
+//
+//  DrinkSizeSwitcher.swift
+//  LemonRestaurantCh11
+//
+//  Created by Stephen Truitt on 7/22/26.
+//
+
+import SwiftUI
+
+struct DrinkSizeSwitcher: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrinkSizeSwitcher()
+}
